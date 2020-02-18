7358e0742f56bac7edbdd0d1641f73fa67d7403b
shubham <you@example.com>
2020-02-18 18:10:10 +0530

    pushing project

---
+++ src/main/java/com/mygit/qa/first/MyfirstDemo.java
@@ -0,0 +1,8 @@
+package com.mygit.qa.first;
+
+public class MyfirstDemo {
+	public static void main(String[] args) {
+		System.out.println("im in my firstdemo");
+	}
+
+}
---
+++ src/main/java/com/mygit/qa/second/MysecondDemo.java
@@ -0,0 +1,8 @@
+package com.mygit.qa.second;
+
+public class MysecondDemo {
+	public static void main(String[] args) {
+		System.out.println("ankicha mutra");
+	}
+
+}
